d6ccae5764d257011ef3919e5c7b0567e611f64a
Ayman <mcaymann@gmail.com>
2022-06-18 23:51:39 +0300

    add more changes to settings UI

--- lib/widgets/settings_bottom_sheet.dart
+++ lib/widgets/settings_bottom_sheet.dart
@@ -47,9 +47,6 @@ class SettingsBottomSheet extends StatelessWidget {
                 ),
               ],
             ),
-            const SizedBox(
-              height: 5.0,
-            ),
             const Text(
               'Theme',
               style: TextStyle(
@@ -58,6 +55,67 @@ class SettingsBottomSheet extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            SizedBox(
+              height: 50,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 14.0),
+                    child: Stack(
+                      clipBehavior: Clip.antiAlias,
+                      children: [
+                        Container(
+                          width: 50.0,
+                          height: 50.0,
+                          decoration: const ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                color: Colors.green,
+                                width: 1.0,
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10.0),
+                            ),
+                            child: Image.asset(
+                              'assets/images/background_1.jpeg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 0,
+                          right: 0,
+                          child: Container(
+                            height: 15.0,
+                            width: 15.0,
+                            decoration: const ShapeDecoration(
+                              shape: CircleBorder(),
+                              color: Colors.green,
+                            ),
+                            child: const Icon(
+                              Icons.done_outlined,
+                              color: Colors.white,
+                              size: 10.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
