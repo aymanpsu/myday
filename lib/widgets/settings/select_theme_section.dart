dbb86f4383d7932959d27212e49ea0198208ba7e
Ayman <mcaymann@gmail.com>
2022-09-23 23:09:04 +0300

    add theme list UI

--- lib/widgets/settings/select_theme_section.dart
+++ lib/widgets/settings/select_theme_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'background_theme_item.dart';
 
 class SelectTheme extends StatelessWidget {
   const SelectTheme({Key? key}) : super(key: key);
@@ -6,6 +7,7 @@ class SelectTheme extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const SizedBox(
           height: 15.0,
@@ -22,60 +24,23 @@ class SelectTheme extends StatelessWidget {
           height: 15.0,
         ),
         SizedBox(
-          height: 70,
+          height: 50,
           child: ListView(
             scrollDirection: Axis.horizontal,
             shrinkWrap: true,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 14.0),
-                child: Stack(
-                  // clipBehavior: Clip.antiAlias,
-                  children: [
-                    Container(
-                      width: 50.0,
-                      height: 50.0,
-                      decoration: const ShapeDecoration(
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(
-                            color: Colors.green,
-                            width: 1.0,
-                          ),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10.0),
-                          ),
-                        ),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(10.0),
-                        ),
-                        child: Image.asset(
-                          'assets/images/background_1.jpeg',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      right: 0,
-                      child: Container(
-                        height: 15.0,
-                        width: 15.0,
-                        decoration: const ShapeDecoration(
-                          shape: CircleBorder(),
-                          color: Colors.green,
-                        ),
-                        child: const Icon(
-                          Icons.done_outlined,
-                          color: Colors.white,
-                          size: 10.0,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+            children: const [
+              BackgroundThemeItem(
+                selectedTheme: true,
+                themeImage: 'assets/images/background_1.jpeg',
               ),
+              BackgroundThemeItem(
+                selectedTheme: false,
+                themeImage: 'assets/images/background_2.jpeg',
+              ),
+              BackgroundThemeItem(
+                selectedTheme: false,
+                themeImage: 'assets/images/background_3.jpeg',
+              )
             ],
           ),
         )
